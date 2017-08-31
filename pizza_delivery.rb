8f77e358fe7d918661c2950d1e851546fe3c33da
Matthijs <matthijs_mandjes@hotmail.com>
2017-08-31 16:17:50 +0200

    pizza delivery

--- pizza_delivery.rb
+++ pizza_delivery.rb
@@ -1,4 +1,9 @@
+require_relative "menu.rb"
+require_relative "order.rb"
+
 done = false
+order = Order.new
+menu = Menu.new
 
 puts "Hello, welcome to our pizza express delivery"
 
@@ -6,14 +11,25 @@ while not done
   puts "What can I do for you?"
   puts "1: Order a pizza"
   puts "2: Nothing"
+  puts "3: Check out (€ #{order.value})" if order.value > 0
   puts "------------------------------"
 
   choice = gets.chomp.to_i
   case choice
     when 1
       puts "Let me show you the menu"
+      menu.print
+      pizza = menu.make_choice
+      order.add(pizza)
     when 2
       done = true
+    when 3
+      order.print
+      puts "Do you wish to pay with"
+      puts "1: ideal"
+      puts "2: creditcard"
+      payment = gets.chomp.to_i
+      done = (payment == 1 || payment == 2)
     else
       puts "I dont understand"
   end
